5dc3d40be4d986ede66d3a81fb5744ad51e72809
Victor del Valle <vicalle49@gmail.com>
2020-02-02 01:56:21 +0100

    add fake table

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App."/>
+    <v-app>
+      <v-container><List/></v-container>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    List,
   }
 }
 </script>
--- front/src/main.js
+++ front/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+Vue.use(Vuex);
 
 import VueRouter from 'vue-router';
 
@@ -11,5 +17,6 @@ import router from "./router";
 
 new Vue({
   router,
+  vuetify: new Vuetify(),
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
